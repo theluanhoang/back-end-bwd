165229438b733929e38c0a64d23bddfe5343e33a
LuanCodeDao <hoangtheluan2016@gmail.com>
2022-07-26 15:13:35 +0700

    upload

--- src/app/models/Doctor.js
+++ src/app/models/Doctor.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema;
 const Doctor = new Schema({
     IdCard: {type: String, maxlength: 255},
     phoneNumber: {type: String, maxlength: 255},
+    name: {type: String, maxlength: 255},
     role: {type: String, maxlength: 255},
     password: {type: String},
     image: {type: String}
