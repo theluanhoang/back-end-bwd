7ecd8ccab52823834bd3efcf919a1231c8d4c254
LuanCodeDao <hoangtheluan2016@gmail.com>
2022-07-26 15:52:12 +0700

    upload

--- src/app/models/Doctor.js
+++ src/app/models/Doctor.js
@@ -6,6 +6,10 @@ const Doctor = new Schema({
     IdCard: {type: String, maxlength: 255},
     phoneNumber: {type: String, maxlength: 255},
     name: {type: String, maxlength: 255},
+    gender: {type: String, maxlength: 255},
+    address: {type: String, maxlength: 255},
+    dateOfBirth: {type: String, maxlength: 255},
+    name: {type: String, maxlength: 255},
     role: {type: String, maxlength: 255},
     password: {type: String},
     image: {type: String}
