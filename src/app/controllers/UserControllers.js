d0cdec3d68c59ec2e95c6241c136b7ec8254b02b
LuanCodeDao <hoangtheluan2016@gmail.com>
2022-07-28 14:25:21 +0700

    upload

--- src/app/controllers/UserControllers.js
+++ src/app/controllers/UserControllers.js
@@ -9,20 +9,19 @@ class UserControllers {
         res.send(result)
     }
 
-    async sendQR(req, res) {
+    sendQR(req, res) {
         const sid = 'AC9cbaaef6bcd414a7557c613ee65290a1'
         const auth_token = '8898cb3229f2e8b6d1244a7b53f3266b'
         const twilio = require('twilio')(sid, auth_token)
-        let otp = await Math.floor(Math.random() * 999999) + 100000;
+        let otp = Math.floor(Math.random() * 999999) + 100000;
 
-        res.send(otp)
-        // twilio.messages.create({
-        //     from: "+13187082606",
-        //     to: '+84379124695',
-        //     body: 'OTP: 185324'
-        // })
-        //     .then((res) => console.log('message has sent!'))
-        //     .catch((err) => console.log(err))
+        twilio.messages.create({
+            from: "+13187082606",
+            to: '+84379124695',
+            body: `OTP: ${otp}`
+        })
+            .then((res) => console.log('message has sent!'))
+            .catch((err) => console.log(err))
     }
 
     // [GET] /
