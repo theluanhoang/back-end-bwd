17f5476255ce549a0dd6b4449c444dc75b8ea48e
LuanCodeDao <hoangtheluan2016@gmail.com>
2022-07-28 13:56:16 +0700

    upload

--- src/app/controllers/UserControllers.js
+++ src/app/controllers/UserControllers.js
@@ -9,6 +9,16 @@ class UserControllers {
         res.send(result)
     }
 
+    async sendQR(req, res) {
+        twilio.messages.create({
+            from: "+13187082606",
+            to: '+84379124695',
+            body: 'OTP: 185324'
+        })
+            .then((res) => console.log('message has sent!'))
+            .catch((err) => console.log(err))
+    }
+
     // [GET] /
     async show(req, res) {
         let user = await User.find();
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const sid = 'AC9cbaaef6bcd414a7557c613ee65290a1'
+const auth_token = '5abef8103def345fa643ba5b381bfed7'
+const twilio = require('twilio')(sid, auth_token)
 
 const userControllers = require('../app/controllers/UserControllers')
 
 router.post('/signup', userControllers.signup)
 router.get('/', userControllers.show)
+router.get('/sendOTP', userControllers.sendQR)
 
 module.exports = router
