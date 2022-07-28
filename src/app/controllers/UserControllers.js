b7f60c348098bd5a5496b5f537d6338fefcd9eda
LuanCodeDao <hoangtheluan2016@gmail.com>
2022-07-28 14:02:06 +0700

    upload

--- src/app/controllers/UserControllers.js
+++ src/app/controllers/UserControllers.js
@@ -10,6 +10,10 @@ class UserControllers {
     }
 
     async sendQR(req, res) {
+        const sid = 'AC9cbaaef6bcd414a7557c613ee65290a1'
+        const auth_token = '5abef8103def345fa643ba5b381bfed7'
+        const twilio = require('twilio')(sid, auth_token)
+
         twilio.messages.create({
             from: "+13187082606",
             to: '+84379124695',
