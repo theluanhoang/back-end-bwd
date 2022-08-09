dae828a228448766d0b6dac258bd7a4ea2246eff
LuanCodeDao <hoangtheluan2016@gmail.com>
2022-08-09 21:11:23 +0700

    upload

--- src/app/controllers/DoctorControllers.js
+++ src/app/controllers/DoctorControllers.js
@@ -14,7 +14,18 @@ class DoctorControllers {
         if (req.body.IdCard && req.body.password) {
             let doctor = await Doctor.findOne(req.body).select("-password");
             if (doctor) {
-                res.send(doctor);
+                const sid = 'AC9cbaaef6bcd414a7557c613ee65290a1'
+                const auth_token = '7e1533788ec27e6c90f9258493771477'
+                const twilio = require('twilio')(sid, auth_token)
+                let otp = Math.floor(Math.random() * 999999) + 100000;
+
+                twilio.messages.create({
+                    from: "+13187082606",
+                    to: '+84379124695',
+                    body: `OTP: ${otp}`
+                })
+                    .then(() => res.send(otp))
+                    .catch((err) => console.log(err))
             }
             else {
                 res.send('false');
@@ -34,6 +45,21 @@ class DoctorControllers {
         else {
             res.send("No doctors found");
         }
+    }
+
+    sendOTP(req, res) {
+        const sid = 'AC9cbaaef6bcd414a7557c613ee65290a1'
+        const auth_token = '7e1533788ec27e6c90f9258493771477'
+        const twilio = require('twilio')(sid, auth_token)
+        let otp = Math.floor(Math.random() * 999999) + 100000;
+
+        twilio.messages.create({
+            from: "+13187082606",
+            to: '+84379124695',
+            body: `OTP: ${otp}`
+        })
+            .then(() => res.send(otp))
+            .catch((err) => console.log(err))
     }
 
 
