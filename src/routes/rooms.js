0a121ee808a7a1223ad57d0419023860112baaa0
LuanCodeDao <hoangtheluan2016@gmail.com>
2022-08-11 08:28:45 +0700

    upload

--- src/routes/rooms.js
+++ src/routes/rooms.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 const roomControllers = require('../app/controllers/RoomControllers')
 
-router.post('/add', roomControllers.create)
+router.post('/create', roomControllers.create)
 router.get('/', roomControllers.show)
 
 module.exports = router
