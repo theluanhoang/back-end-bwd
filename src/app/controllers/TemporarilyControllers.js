0bbe431444d7c6133a7b8d2ff883c57249124541
LuanCodeDao <hoangtheluan2016@gmail.com>
2022-08-13 09:49:21 +0700

    upload

--- src/app/controllers/TemporarilyControllers.js
+++ src/app/controllers/TemporarilyControllers.js
@@ -21,6 +21,24 @@ class TemporarilyControllers {
         }
     }
 
+    // [GET] Gửi tin nhắn
+    sendSMS(req, res) {
+        const dateStart = req.body.NgayBatDauTamVang;
+        const dateEnd = req.body.NgayKetThucTamVang;
+        const sid = 'AC9cbaaef6bcd414a7557c613ee65290a1'
+        const auth_token = '2d399a856684220cd1fcd1e381c9217f'
+        const twilio = require('twilio')(sid, auth_token)
+        let otp = Math.floor(Math.random() * 999999) + 100000;
+
+        twilio.messages.create({
+            from: "+13187082606",
+            to: '+84379124695',
+            body: `Yêu cầu đăng ký tạm vắng của bạn đã được chấp thuận. Thời hạn tạm vắng của bạn bắt đầu từ ngày ${dateStart} đến hết ngày ${dateEnd}`
+        })
+            .then(() => res.send(otp))
+            .catch((err) => console.log(err))
+    }
+
 
 }
 
