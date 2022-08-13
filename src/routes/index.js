5313043f41b6de9eba451073b7e65d9dc85eeee3
LuanCodeDao <hoangtheluan2016@gmail.com>
2022-08-13 09:06:32 +0700

    upload

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,14 @@ const doctorsRouter = require('./doctors')
 const patientIdRouter = require('./patientId')
 const userRouter = require('./users')
 const roomRouter = require('./rooms')
+const temporarilyRouter = require('./temporarily')
 
 function route(app) {
     app.use('/doctors', doctorsRouter)
     app.use('/patientId', patientIdRouter)
     app.use('/users', userRouter)
     app.use('/rooms', roomRouter)
+    app.use('/temporarily', temporarilyRouter)
 }
 
 module.exports = route
