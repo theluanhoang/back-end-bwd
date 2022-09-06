8590145053fc5a0b4d54f53739c0a9ef256a04c8
LuanCodeDao <hoangtheluan2016@gmail.com>
2022-09-06 22:13:22 +0700

    upload

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,11 +4,11 @@ const userRouter = require('./users')
 const roomRouter = require('./rooms')
 const temporarilyRouter = require('./temporarily')
 const stayingRouter = require('./staying')
-const conversationsRouter = require('./conversations')
+// const conversationsRouter = require('./conversations')
 
 function route(app) {
     app.use('/doctors', doctorsRouter)
-    app.use('/conversation', conversationsRouter)
+    // app.use('/conversation', conversationsRouter)
     app.use('/patientId', patientIdRouter)
     app.use('/users', userRouter)
     app.use('/rooms', roomRouter)
