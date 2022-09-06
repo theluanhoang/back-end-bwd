329f7a9cd43687222e312df0ea91c052f233db15
LuanCodeDao <hoangtheluan2016@gmail.com>
2022-09-06 11:38:58 +0700

    upload

--- src/app/controllers/DoctorControllers.js
+++ src/app/controllers/DoctorControllers.js
@@ -70,7 +70,7 @@ class DoctorControllers {
     sendSMS(req, res) {
         let otp = Math.floor(Math.random() * 999999) + 100000;
         let str = req.body.phoneNumber + ": " + otp;
-        res.send(otp);
+        res.send(str);
     }
 }
 
