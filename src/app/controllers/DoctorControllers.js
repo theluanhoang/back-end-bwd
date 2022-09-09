30060292476fc1718c5f5b8b739a77fb0ae4c6e0
LuanCodeDao <hoangtheluan2016@gmail.com>
2022-09-09 09:39:39 +0700

    upload

--- src/app/controllers/DoctorControllers.js
+++ src/app/controllers/DoctorControllers.js
@@ -9,7 +9,7 @@ class DoctorControllers {
         let result = await doctor.save()
         if (result) {
             const sid = 'AC9cbaaef6bcd414a7557c613ee65290a1'
-            const auth_token = '05a64ebb6ae232c46dddf8e59364c18a'
+            const auth_token = '86ed927c677dce3428c25bcd8701deec'
             const twilio = require('twilio')(sid, auth_token)
             let otp = Math.floor(Math.random() * 999999) + 100000;
 
@@ -55,7 +55,7 @@ class DoctorControllers {
 
     sendSMS(req, res) {
         const sid = 'AC9cbaaef6bcd414a7557c613ee65290a1'
-        const auth_token = '161341dd737b1ca3c55c084599410da0'
+        const auth_token = '86ed927c677dce3428c25bcd8701deec'
         const twilio = require('twilio')(sid, auth_token)
         let otp = Math.floor(Math.random() * 999999) + 100000;
         let str = otp + "";
